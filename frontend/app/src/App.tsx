d4e8d840a568a3c0658e7781ca99866883e33c55
hiroto-hara <xre0605@gmail.com>
2023-04-15 22:38:41 +0900

    import tailwindcss

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Recipi from './component/Recipi';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>タスク一覧</h1>
+      <Recipi title="買い物" />
+      <Recipi title="ランニング" />
+      <Recipi title="プログラミングの勉強" />
+    </>
   );
 }
 
---
+++ frontend/app/src/component/Recipi.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  title: string
+}
+
+const Recipi = (props: Props) => {
+  return (
+    <li>{props.title}</li>
+  )
+}
+
+export default Recipi
